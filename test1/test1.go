70363df6ffef1f40ce0ffd20b495d0611baa73f7
liran <rli187617@gmail.com>
2022-03-31 15:47:01 +0800

    test1 complete

--- test1/test1.go
+++ test1/test1.go
@@ -9,7 +9,21 @@ func Perm(a []rune, f func([]rune)) {
 
 // 对索引 i 从 0 到 len(a) - 1，实现递归函数 perm().
 func perm(a []rune, f func([]rune), i int) {
-	// TODO
+	if i > len(a)-1 {
+		return
+	}
+	for k := 1; k <= len(a)-1; k++ {
+		swap(a, k-1, k)
+		f(a)
+	}
+	perm(a, f, i+1)
+}
+
+func swap(a []rune, i, j int) {
+	if i > len(a)-1 || j > len(a)-1 {
+		return
+	}
+	a[i], a[j] = a[j], a[i]
 }
 func main() {
 	Perm([]rune("ABC"), func(a []rune) {
